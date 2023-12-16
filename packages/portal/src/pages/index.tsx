e1d9be3d83795494b744d6c9a87dbb9dc0acc1f9
Mingyang-Li <mingyangli118@gmail.com>
2023-12-16 23:25:22 +1300

    fix: misc

--- packages/portal/src/pages/index.tsx
+++ packages/portal/src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Button } from '@nextui-org/react';
+import { Button, Input } from '@nextui-org/react';
 
 export default function Index() {
   return (
     <div>
       <Button color="primary">Click me</Button>
+      <Input type="email" label="Email" />
     </div>
   );
 }
