cb6c16fe189fb9df35b7db5d80829613856530cd
Mingyang-Li <mingyangli118@gmail.com>
2023-12-23 00:59:49 +1300

    db: errors saying docker mongodb doesnt have replica-set, idk why

--- packages/portal/scripts/db-seed.ts
+++ packages/portal/scripts/db-seed.ts
@@ -1,8 +1,37 @@
 import * as f from '@ngneat/falso';
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const seedDb = async () => {
   console.log(`Seeding database`);
-  console.log(`firstName => ${f.randFirstName()}`);
+  await createMembers();
 };
 
-seedDb();
+export const createMembers = async () => {
+  let members: Prisma.MemberCreateManyInput[] = [];
+  for (let i = 1; i <= 50; i++) {
+    const firstName = f.randFirstName();
+    const lastName = f.randLastName();
+    const member: Prisma.MemberCreateManyInput = {
+      firstName,
+      lastName,
+      email: `${firstName}-${lastName}-${i}@testing.com`,
+    };
+    members = [...members, member];
+  }
+  const created = await prisma.member.createMany({
+    data: members,
+  });
+  console.log(`Number of members created: ${created.count}`);
+};
+
+seedDb()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
