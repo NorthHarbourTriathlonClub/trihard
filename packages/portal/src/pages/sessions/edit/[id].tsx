559b46b50a36a2a150f49da89989c957a80096f1
Mingyang-Li <mingyangli118@gmail.com>
2023-12-28 10:55:47 +1300

    chore: format

--- packages/portal/src/pages/sessions/edit/[id].tsx
+++ packages/portal/src/pages/sessions/edit/[id].tsx
@@ -1,14 +1,14 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 const EditTrainingSession = () => {
-  const { query } = useRouter()
-  const { id } = query
+  const { query } = useRouter();
+  const { id } = query;
   return (
     <>
       <h1>EditTrainingSession</h1>
       <p>ID: {id}</p>
     </>
-  )
-}
+  );
+};
 
 export default EditTrainingSession;
