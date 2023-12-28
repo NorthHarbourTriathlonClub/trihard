10934b88e2bb1d7b102f1fc36c01da2380ace399
Mingyang-Li <mingyangli118@gmail.com>
2023-12-28 21:22:02 +1300

    feat: integrated clerk for e2e auth

--- packages/portal/src/pages/_app.tsx
+++ packages/portal/src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import { type AppType } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ClerkProvider } from '@clerk/nextjs';
 import { api } from '@/utils/api';
 import '@/styles/global.css';
 
 const App: AppType = ({ Component, pageProps }) => {
   return (
-    <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="dark">
-        <Component {...pageProps} />
-      </NextThemesProvider>
-    </NextUIProvider>
+    <ClerkProvider>
+      <NextUIProvider>
+        <NextThemesProvider attribute="class" defaultTheme="dark">
+          <Component {...pageProps} />
+        </NextThemesProvider>
+      </NextUIProvider>
+    </ClerkProvider>
   );
 };
 
