9f6f223287f6e1eed0d5fac92cbbc25f3da5b761
Mingyang-Li <mingyangli118@gmail.com>
2024-01-11 15:46:08 +1300

    feat: home page entry animation

--- packages/portal/src/pages/index.tsx
+++ packages/portal/src/pages/index.tsx
@@ -1,18 +1,37 @@
 import { Button } from '@nextui-org/react';
-import { NavBarResponsive } from '@/components/NavBarResponsive';
 import { Center, Flex } from '@chakra-ui/layout';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import { routes } from '@/constants/routes';
+import { NavBarResponsive } from '@/components/NavBarResponsive';
 
 const Index = () => {
   return (
     <div>
       <NavBarResponsive />
-      <Flex minH={'80vh'} align={'center'} justify={'center'}>
-        <Center gap={8}>
+      <Flex
+        minH={'80vh'}
+        align={'center'}
+        justify={'center'}
+        direction={'column'}
+      >
+        <Center gap={8} flexDirection={'column'}>
           {routes.map((d, _i) => (
             <Link key={_i} href={d.path}>
-              <Button color={'primary'}>{d.label}</Button>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={{
+                  hidden: { x: -((_i + 1) * 100), opacity: 0 },
+                  visible: { x: 0, opacity: 1, transition: { duration: 0.8 } },
+                }}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <Button color={'primary'} radius={'full'} size={'lg'}>
+                  {d.label}
+                </Button>
+              </motion.div>
             </Link>
           ))}
         </Center>
