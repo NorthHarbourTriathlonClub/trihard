6dae8278901156a46b45738e07ca6932377e7d4f
Mingyang-Li <mingyangli118@gmail.com>
2024-02-11 22:28:47 +1300

    cleanup: remove unused route for concession-cards

--- packages/portal/src/server/root.ts
+++ packages/portal/src/server/root.ts
@@ -1,5 +1,4 @@
 import { athleteRoutes } from '@/server/routes/athlete.routes';
-import { concessionCardRoutes } from '@/server/routes/concession-card.routes';
 import { signInAthleteRoutes } from '@/server/routes/sign-in-athlete.routes';
 import { trainingSessionRoutes } from '@/server/routes/training-session.routes';
 import { createTRPCRouter } from '@/server/trpc';
@@ -8,7 +7,6 @@ export const appRouter = createTRPCRouter({
   athleteRoutes,
   trainingSessionRoutes,
   signInAthleteRoutes,
-  concessionCardRoutes,
 });
 
 export type AppRouter = typeof appRouter;
