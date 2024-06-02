71e789fcc903e036e467916ed14cd40648a09e10
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2024-06-02 17:06:07 +1200

    use common service for crud

--- packages/core-services/src/services/athlete.service.ts
+++ packages/core-services/src/services/athlete.service.ts
@@ -1,19 +1,5 @@
 import { z } from 'zod';
-
-/**
- * Useful for validating IDs that are strings
- *
- * During CRUD operations, we must use an ID that's
- * not an empty string, and does not contain any
- * space characters, because our database doesn't
- * accept IDs with the above attributes in the first
- * place, therefore we need to validate the IDs before
- * we make a trip to the database to save costs
- */
-export const IdSchema = z
-  .string()
-  .min(2, { message: `IDs must contain more than 2 characters` })
-  .regex(/^\S*$/, `IDs must not contain spaces`);
+import { IdSchema } from './common.service';
 
 export const SignInToTrainingArgsSchema = z.object({
   athleteId: IdSchema,
