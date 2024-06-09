89ac62c35edbac98ea54ecd8be901d662a4c2963
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2024-06-09 12:25:48 +1200

    fix prisma type export from @core/db, bumped prisma pkg versions

--- packages/core-db/src/index.ts
+++ packages/core-db/src/index.ts
@@ -1,11 +1,7 @@
-import { Prisma, PrismaClient } from '@prisma/client';
-
 /**
- * exports all type definitions that '@prisma/client' contains (but does not export)
+ * exports all type definitions that '@prisma/client' contains
  *
  * Purpose: allows apps & packages that use '@core/db' to access the same
  * types provided by '@prisma/client'
  */
-export * from '../node_modules/@prisma/client/index';
-
-export { Prisma, PrismaClient };
+export * from '@prisma/client';
