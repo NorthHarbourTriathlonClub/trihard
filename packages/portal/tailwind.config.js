6d116444bfdf22a45ee9b4a5c47aeebf37d2b7d9
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2024-06-09 12:33:16 +1200

    applied eslint to all pkgs

--- packages/portal/tailwind.config.js
+++ packages/portal/tailwind.config.js
@@ -3,9 +3,9 @@ import { nextui } from '@nextui-org/react';
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    "./node_modules/tailwind-datepicker-react/dist/**/*.js",
+    './node_modules/tailwind-datepicker-react/dist/**/*.js',
   ],
   theme: {
     extend: {},
